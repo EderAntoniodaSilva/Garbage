c167671c38ea904049e995927994c2c731ed6ac5
Marcelo Sousa Candido <msc18a@gmail.com>
2017-06-13 11:51:19 -0300

    modificações no jogo em sala de aula

--- Garbage/Assets/Scripts/CameraController.cs
+++ Garbage/Assets/Scripts/CameraController.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject itemPapel;
+	public GameObject itemVidro;
+	private List<Vector3> gridPositions = new List<Vector3> ();
 	private Vector3 offset;
 
 	// Use this for initialization
@@ -16,4 +20,39 @@ public class CameraController : MonoBehaviour {
 	void LateUpdate () {
 		transform.position = player.transform.position + offset;
 	}
+
+	/* PENSAR NESSA PARTE, PARA COLOCAR AUTOMATICO OS ITENS A COLETAR Parte 3 do documento de apoio.
+	Vector3 RandomPosition(){
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions [randomIndex];
+		gridPositions.RemoveAt (randomIndex);
+		return randomPosition;
+	}
+
+	void LayoutObjectAtRandom(GameObject objectToPut, int minimum, int maximum){
+		int objectCount = Random.Range (minimum, maximum + 1);
+		for (int i = 0; i < objectCount; i++) {
+			Vector3 randomPosition = RandomPosition ();
+			Instantiate (objectToPut, randomPosition, Quaternion.identity);
+		}
+	}
+
+	void InitialiseList() {
+		float posX = 0.0f;
+		for (int x = 1; x <= 7; x++) {
+			float posY = 0.0f;
+			for (int y = 1; y <= 7; y++) {
+				gridPositions.Add (new Vector3 (posX, posY, 0f));
+				posY += 0.0f;
+			}
+			posX += 0.0f;
+		}
+	}
+
+	void Awake() {
+		InitialiseList ();
+		LayoutObjectAtRandom (itemPapel, 5, 5);
+		LayoutObjectAtRandom (itemVidro, 5, 5);
+	}
+	*/
 }
--- Garbage/Assets/Scripts/PlayerController.cs
+++ Garbage/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,30 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text finalText;
 	private Rigidbody2D rb2d;
+	private bool facingRight;
+	private int count;
+	public int vidas;
+	public Text textoVidas;
 
 	// Use this for initialization
 	void Start () {
 		
-		rb2d = GetComponent<Rigidbody2D> ();	
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		countText.text = "Papeis Coletados: " + count.ToString ();
+		finalText.text = "";
+		vidas = 3;
+		textoVidas.text = "Vidas: " + vidas;
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 	
 	void FixedUpdate () {
@@ -26,6 +44,24 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, jump, moveVertical);
 
 		rb2d.AddForce (movement * speed);
-		jump = 0;
+		if (moveHorizontal > 0 && facingRight)
+			Flip ();
+		else if (moveHorizontal < 0 && !facingRight)
+			Flip ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("ItemPapel")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			MarcarPonto ();
+		}
+	}
+
+	void MarcarPonto(){
+		countText.text = "Papeis Coletados: " + count.ToString ();
+		if (count >= 10) {
+			finalText.text = "Você Venceu!";
+		}
 	}
 }
